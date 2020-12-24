010758e9fbc81177963465cd77a44e1cbf0c7ad1
Mahsum Kocabey <mahsumkocabey@hotmail.com>
2020-12-24 17:24:02 +0000

    homepage created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 // import logo from './logo.svg';
 import React, { Component } from 'react';
-import { CardList } from './components/card-list/card-list.component'
-import { SearchBox } from './components/search-box/search-box.component'
-
+import HomePage from './homepage.component';
 import './App.css'
 
 class App extends Component {
@@ -10,40 +8,18 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [],
-      searchField: ''
+
     }
     /* Commented this bind out because we call es6 arrow function for the handleChange method. */
     // this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users}))
-  }
-
-  handleChange = e => {
-    this.setState({ searchField: e.target.value })
-  }
 
   render(){
-    // Getting rid of using this.state everytime.
-    const { monsters, searchField } = this.state;
-
-    // Creating a filter function for the search box.
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    )
 
     return (
-      <div className="App">
-        <h1>Monsters Roladex</h1>        
-        <SearchBox
-          placeholder = 'Search Monsters'
-          handleChange = { this.handleChange }
-        />
-        <CardList monsters={ filteredMonsters }/>
+      <div>
+        <HomePage />
       </div>
     )
   }
