a8a2b1e0ded11edbb8b0cd8e650e0e8a5ab31d55
Mahsum Kocabey <mahsumkocabey@hotmail.com>
2020-12-25 13:16:01 +0000

    broken our hompage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import React, { Component } from 'react';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css'
 
 class App extends Component {
